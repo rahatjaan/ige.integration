db6958187109c08eb8980d886e03b807494e8f3e
Rahat Ali <rahat.jaan@gmail.com>
2013-10-18 18:04:10 +0500

    As of Oct 18, 2013

--- src/main/java/ige/integration/transformer/GuestTransactionsTransformer.java
+++ src/main/java/ige/integration/transformer/GuestTransactionsTransformer.java
@@ -9,22 +9,19 @@ import org.xml.sax.SAXException;
 public class GuestTransactionsTransformer {
 	public static String transform(String message, boolean flag) throws ParserConfigurationException, SAXException, IOException{
 		if(flag){
-			int ind1 = message.indexOf("<soap:Fault");
-			int ind2 = message.indexOf("</soap:Fault>");
-			message = message.substring(ind1,ind2);
-			ind1 = message.indexOf("<faultcode>");
-			message = message.substring(ind1);
-			message = "<guestInfos><ServiceError>"+message+"</ServiceError></guestInfos>";
+			if(message.contains("guestCheckoutFALSE")){
+				message = "<guestInfos><ServiceError>"+message+"</ServiceError></guestInfos>";
+			}else{
+				int ind1 = message.indexOf("<soap:Fault");
+				int ind2 = message.indexOf("</soap:Fault>");
+				message = message.substring(ind1,ind2);
+				ind1 = message.indexOf("<faultcode>");
+				message = message.substring(ind1);
+				message = "<guestInfos><ServiceError>"+message+"</ServiceError></guestInfos>";
+			}
 			System.out.println("Message is: "+message);
 		}else{
-			int ind1 = message.indexOf("<return");
-			int ind2 = message.indexOf("</return>");
-			message = message.substring(ind1+3,ind2);
-			System.out.println("ROUGH: "+message);
-			ind1 = message.indexOf("<");
-			System.out.println("INDEX: "+ind1);
-			message = message.substring(ind1);
-			message = "<guestInfos>"+message+"</guestInfos>";
+			message = "<guestInfos><Success>"+"Successfully Checked Out"+"</Success></guestInfos>";
 			System.out.println("Message is: "+message);
 			//new GenerateReport().generateReport(message);
 		}
